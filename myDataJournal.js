7497587f9a56534d15fc0d2eb09b5e9dbab9012e
Beatriz Galarza <beatriz.glrz@gmail.com>
2026-01-30 21:59:00 -0800

    Added predictions before coding

---
+++ myDataJournal.js
@@ -0,0 +1,12 @@
+/*
+Beatriz Galarza
+Repository: https://github.com/beaglrz/cs81-module4b-mydataexplorer
+*/
+
+/*Predictions Before Coding
+- Which day had the most screen time? Saturday
+- Best focus day? Friday
+- Is more caffeine helping? I don't believe so
+*/
+
+
