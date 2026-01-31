6a4401e9771da6fa6a6faabf83adbe4f98ef2e13
Beatriz Galarza <beatriz.glrz@gmail.com>
2026-01-30 22:12:45 -0800

    Added weekData array

--- myDataJournal.js
+++ myDataJournal.js
@@ -9,4 +9,13 @@ Repository: https://github.com/beaglrz/cs81-module4b-mydataexplorer
 - Is more caffeine helping? I don't believe so
 */
 
+const weekData = [
+  { day: "Monday", sleepHours: 9, screenTime: 5.5, mood: "busy", caffeineIntake: 1, focusLevel: 6 },
+  { day: "Tuesday", sleepHours: 7, screenTime: 5, mood: "productive", caffeineIntake: 0, focusLevel: 7 },
+  { day: "Wednesday", sleepHours: 6, screenTime: 5.3, mood: "stressed", caffeineIntake: 3, focusLevel: 5 },
+  { day: "Thursday", sleepHours: 7, screenTime: 4.5, mood: "calm", caffeineIntake: 1, focusLevel: 5 },
+  { day: "Friday", sleepHours: 8, screenTime: 2, mood: "focused", caffeineIntake: 0, focusLevel: 8 },
+  { day: "Saturday", sleepHours: 6, screenTime: 6, mood: "tired", caffeineIntake: 0, focusLevel: 7 },
+  { day: "Sunday", sleepHours: 8, screenTime: 4, mood: "relaxed", caffeineIntake: 1, focusLevel: 8 }
+];
 
